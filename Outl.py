adbff0e1fa662b8d53d62e3f50dc213d29719fbe
Bpem9 <itimeq@gmail.com>
2023-03-20 23:55:38 +0200

    Refactored a little

--- Outl.py
+++ Outl.py
@@ -1,31 +1,30 @@
 
+from typing import List
 import numpy
 import pandas as pd
 import win32com.client as client
 
-
-data = pd.read_excel('Входные данные.xlsx', sheet_name='Лист1')
-logins = []
-for login in data['login'].array:
-    if not login in logins:
-        logins.append(login)
-    else:
-        continue
-servers = []
-for server in data['servers'].array:
-    if not server in servers:
-        servers.append(server)
+excel_data = pd.read_excel('Входные данные.xlsx', sheet_name='Лист1')
 
 
-def sending(logins):
-    outlook = client.Dispatch('Outlook.Application')
-    for login in logins:
-        message = outlook.CreateItem(0)
-        message.Display(0)
-        message.To = str(login) + '@gmail.com'
-        message.Subject = 'Test'
-        message.Body = str(data[data.login == login].iloc[:, 1])
+class Receiver:
+    @staticmethod
+    def get_logins(data: List) -> List:
+        return list(set(self.data['login'].array))
 
-    #message.Send()
 
-sending(logins)
+class EnterSendman:
+    @staticmethod
+    def send_to_gmail_for_logins(logins: List) -> None:
+        outlook = client.Dispatch('Outlook.Application')
+        for login in logins:
+            message = outlook.CreateItem(0)
+            message.Display(0)
+            message.To = str(login) + '@gmail.com'
+            message.Subject = 'Test'
+            message.Body = str(data[data.login == login].iloc[:, 1])
+            # message.send()
+
+
+excel_logins = Receiver().get_logins(excel_data)
+EnterSendman.send_to_gmail_for_logins(excel_logins)
